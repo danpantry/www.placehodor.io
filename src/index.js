ab70ffbf7ed3f08ef582a80670de2154783a7cf0
Dan Pantry <devnull@danpantry.me>
2020-04-10 17:04:37 +0100

    promisify pipeline()

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
-import sharp from "sharp";
-import { promises as fs, createReadStream } from "fs";
-import { pipeline } from "stream";
+import { createReadStream, promises as fs } from "fs";
 import path from "path";
+import sharp from "sharp";
+import { pipeline } from "stream";
+import { promisify } from "util";
+const pipe = promisify(pipeline);
 
 const PORT = process.env.PORT ?? 8080;
 const HOSTNAME = process.env.HOSTNAME ?? "localhost";
@@ -13,24 +15,18 @@ app.get("/:width/:height", async (req, res, next) => {
   try {
     const height = parseInt(req.params.height, 10);
     const width = parseInt(req.params.width, 10);
-
     const dir = path.resolve(__dirname, "../static/images");
     const files = await fs.readdir(dir);
     const idx = Math.ceil(Math.random() * files.length - 1);
     const filename = files[idx];
-    pipeline(
-      createReadStream(path.resolve(dir, filename)),
-      sharp().resize(width, height).toFormat("webp"),
-      res,
-      (err) => {
-        if (err != null) {
-          console.log(err);
-          return;
-        }
 
-        res.end();
-      }
+    await pipe(
+      createReadStream(path.resolve(dir, filename), { autoClose: true }),
+      sharp().resize(width, height).toFormat("webp"),
+      res
     );
+
+    res.end();
   } catch (e) {
     next(e);
   }
